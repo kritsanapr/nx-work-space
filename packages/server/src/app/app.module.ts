7b03383b3f2d855f228da82215b7538d7a790d4c
kritsanapr <kritsana.pr.41@gmail.com>
2023-04-27 17:25:13 +0000

    chore : update struture project prisma.

--- packages/server/src/app/app.module.ts
+++ packages/server/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 @Module({
   imports: [],
   controllers: [AppController],
